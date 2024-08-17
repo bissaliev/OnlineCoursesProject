773d64a2f093e9e195970f769bd0f107ab251dd1
bissaliev <bissaliev21@gmail.com>
2024-08-17 09:37:09 +0400

    Создание моделей

--- product/courses/models.py
+++ product/courses/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -6,24 +7,23 @@ class Course(models.Model):
 
     author = models.CharField(
         max_length=250,
-        verbose_name='Автор',
+        verbose_name="Автор",
     )
     title = models.CharField(
         max_length=250,
-        verbose_name='Название',
+        verbose_name="Название",
     )
     start_date = models.DateTimeField(
         auto_now=False,
         auto_now_add=False,
-        verbose_name='Дата и время начала курса'
+        verbose_name="Дата и время начала курса",
     )
-
-    # TODO
+    price = models.PositiveBigIntegerField(verbose_name="Стоимость курса")
 
     class Meta:
-        verbose_name = 'Курс'
-        verbose_name_plural = 'Курсы'
-        ordering = ('-id',)
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ("-id",)
 
     def __str__(self):
         return self.title
@@ -34,19 +34,23 @@ class Lesson(models.Model):
 
     title = models.CharField(
         max_length=250,
-        verbose_name='Название',
+        verbose_name="Название",
     )
     link = models.URLField(
         max_length=250,
-        verbose_name='Ссылка',
+        verbose_name="Ссылка",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="Курс",
     )
 
-    # TODO
-
     class Meta:
-        verbose_name = 'Урок'
-        verbose_name_plural = 'Уроки'
-        ordering = ('id',)
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ("id",)
 
     def __str__(self):
         return self.title
@@ -55,9 +59,23 @@ class Lesson(models.Model):
 class Group(models.Model):
     """Модель группы."""
 
-    # TODO
+    title = models.CharField(
+        max_length=250,
+        verbose_name="Название группы",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="groups",
+        verbose_name="Курс",
+    )
+    students = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="group_students",
+        verbose_name="Студенты",
+    )
 
     class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
-        ordering = ('-id',)
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+        ordering = ("-id",)
--- product/users/models.py
+++ product/users/models.py
@@ -1,26 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
     """Кастомная модель пользователя - студента."""
 
     email = models.EmailField(
-        verbose_name='Адрес электронной почты',
-        max_length=250,
-        unique=True
+        verbose_name="Адрес электронной почты", max_length=250, unique=True
     )
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = (
-        'username',
-        'first_name',
-        'last_name',
-        'password'
-    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username", "first_name", "last_name", "password")
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-        ordering = ('-id',)
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ("-id",)
 
     def __str__(self):
         return self.get_full_name()
@@ -29,21 +23,42 @@ class CustomUser(AbstractUser):
 class Balance(models.Model):
     """Модель баланса пользователя."""
 
-    # TODO
+    student = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="balance",
+        verbose_name="Студент",
+    )
+    bonus = models.PositiveBigIntegerField(default=1000, verbose_name="Бонусы")
 
     class Meta:
-        verbose_name = 'Баланс'
-        verbose_name_plural = 'Балансы'
-        ordering = ('-id',)
+        verbose_name = "Баланс"
+        verbose_name_plural = "Балансы"
+        ordering = ("-id",)
 
 
 class Subscription(models.Model):
     """Модель подписки пользователя на курс."""
 
-    # TODO
+    course = models.ForeignKey(
+        "courses.Course",
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        verbose_name="Курс",
+    )
+    student = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        verbose_name="Студент",
+    )
 
     class Meta:
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
-        ordering = ('-id',)
-
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        ordering = ("-id",)
+        constraints = [
+            models.UniqueConstraint(
+                fields=("course", "student"), name="unique_course_student"
+            )
+        ]
