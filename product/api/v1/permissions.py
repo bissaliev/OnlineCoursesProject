72b48bb42bf4e9b6d0bbbfb58ef4dd4b41d1e191
bissaliev <bissaliev21@gmail.com>
2024-08-17 09:45:21 +0400

    Натсройка разрешений и оплаты

--- product/api/v1/permissions.py
+++ product/api/v1/permissions.py
@@ -1,24 +1,31 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.db.models import F
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 from users.models import Subscription
 
 
-def make_payment(request):
-    # TODO
-    pass
+def make_payment(request, course):
+    request.user.balance.bonus = F("bonus") - course.price
+    request.user.balance.save()
+    request.user.balance.refresh_from_db()
 
 
 class IsStudentOrIsAdmin(BasePermission):
     def has_permission(self, request, view):
-        # TODO
-        pass
+        if request.user.is_staff:
+            return True
+        return Subscription.objects.filter(
+            student=request.user, course__id=view.kwargs.get("course_id")
+        ).exists()
 
     def has_object_permission(self, request, view, obj):
-        # TODO
-        pass
+        if request.user.is_staff:
+            return True
+        return Subscription.objects.filter(
+            student=request.user, course__lessons=obj
+        ).exists()
 
 
 class ReadOnlyOrIsAdmin(BasePermission):
-
     def has_permission(self, request, view):
         return request.user.is_staff or request.method in SAFE_METHODS
 
