037e14bb532d699936a355a7593c0e82bd775c2a
bissaliev <bissaliev21@gmail.com>
2024-08-17 09:38:01 +0400

    Добавление моделей в админку

--- product/courses/admin.py
+++ product/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Group, Lesson
+
+admin.site.register(Course)
+admin.site.register(Group)
+admin.site.register(Lesson)
--- product/users/admin.py
+++ product/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Balance, CustomUser, Subscription
+
+admin.site.register(CustomUser)
+admin.site.register(Balance)
+admin.site.register(Subscription)
