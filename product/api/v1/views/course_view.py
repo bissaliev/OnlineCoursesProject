fd0ad90f2e8edfbab139593f31b2194d3778fbb7
bissaliev <bissaliev21@gmail.com>
2024-08-17 09:43:00 +0400

    Создание вьюсетов для курсов

--- product/api/v1/views/course_view.py
+++ product/api/v1/views/course_view.py
@@ -1,18 +1,22 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets, permissions
-from rest_framework.decorators import action
-from rest_framework.response import Response
-
-from api.v1.permissions import IsStudentOrIsAdmin, ReadOnlyOrIsAdmin
-from api.v1.serializers.course_serializer import (CourseSerializer,
-                                                  CreateCourseSerializer,
-                                                  CreateGroupSerializer,
-                                                  CreateLessonSerializer,
-                                                  GroupSerializer,
-                                                  LessonSerializer)
+from api.v1.permissions import (
+    IsStudentOrIsAdmin,
+    ReadOnlyOrIsAdmin,
+    make_payment,
+)
+from api.v1.serializers.course_serializer import (
+    CourseSerializer,
+    CreateCourseSerializer,
+    CreateGroupSerializer,
+    CreateLessonSerializer,
+    GroupSerializer,
+    LessonSerializer,
+)
 from api.v1.serializers.user_serializer import SubscriptionSerializer
 from courses.models import Course
-from users.models import Subscription
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class LessonViewSet(viewsets.ModelViewSet):
@@ -21,16 +25,16 @@ class LessonViewSet(viewsets.ModelViewSet):
     permission_classes = (IsStudentOrIsAdmin,)
 
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action in ["list", "retrieve"]:
             return LessonSerializer
         return CreateLessonSerializer
 
     def perform_create(self, serializer):
-        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        course = get_object_or_404(Course, id=self.kwargs.get("course_id"))
         serializer.save(course=course)
 
     def get_queryset(self):
-        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        course = get_object_or_404(Course, id=self.kwargs.get("course_id"))
         return course.lessons.all()
 
 
@@ -40,41 +44,45 @@ class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAdminUser,)
 
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action in ["list", "retrieve"]:
             return GroupSerializer
         return CreateGroupSerializer
 
     def perform_create(self, serializer):
-        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        course = get_object_or_404(Course, id=self.kwargs.get("course_id"))
         serializer.save(course=course)
 
     def get_queryset(self):
-        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        course = get_object_or_404(Course, id=self.kwargs.get("course_id"))
         return course.groups.all()
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    """Курсы """
+    """Курсы"""
 
     queryset = Course.objects.all()
     permission_classes = (ReadOnlyOrIsAdmin,)
 
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action in ["list", "retrieve"]:
             return CourseSerializer
         return CreateCourseSerializer
 
     @action(
-        methods=['post'],
+        methods=["post"],
         detail=True,
-        permission_classes=(permissions.IsAuthenticated,)
+        permission_classes=(permissions.IsAuthenticated,),
     )
     def pay(self, request, pk):
         """Покупка доступа к курсу (подписка на курс)."""
 
-        # TODO
-
-        return Response(
-            data=data,
-            status=status.HTTP_201_CREATED
-        )
+        course = get_object_or_404(Course, pk=pk)
+        data = {"course": course.id, "student": request.user.id}
+        serializer = SubscriptionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            make_payment(request, course)
+            return Response(
+                data=serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=404)
