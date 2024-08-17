3cfc21d030876b35e189e691cc88ed84f9f22ddc
bissaliev <bissaliev21@gmail.com>
2024-08-17 09:41:32 +0400

    Добавление url для админ-панели

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import include, path
 
-app_name = 'api'
+app_name = "api"
 
 urlpatterns = [
-    path('api/v1/', include('api.v1.urls')),
+    path("api/v1/", include("api.v1.urls")),
+    path("admin/", admin.site.urls),
 ]
