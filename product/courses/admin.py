6e42b14d03b8677cdbccbbe63890262dc8a2cb2a
bissaliev <bissaliev21@gmail.com>
2024-08-18 06:13:14 +0400

    Редактирование админ-панели

--- product/courses/admin.py
+++ product/courses/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 
 from .models import Course, Group, Lesson
 
-admin.site.register(Course)
-admin.site.register(Group)
-admin.site.register(Lesson)
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+
+
+class GroupInline(admin.TabularInline):
+    model = Group
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "available", "price", "start_date"]
+    list_editable = ["available", "price", "start_date"]
+    inlines = [LessonInline, GroupInline]
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ["title", "course"]
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["title", "course"]
--- product/users/admin.py
+++ product/users/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 
 from .models import Balance, CustomUser, Subscription
 
-admin.site.register(CustomUser)
-admin.site.register(Balance)
-admin.site.register(Subscription)
+
+class BalanceInline(admin.TabularInline):
+    model = Balance
+
+
+class SubscriptionInline(admin.TabularInline):
+    model = Subscription
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email"]
+    inlines = [BalanceInline, SubscriptionInline]
+
+
+@admin.register(Balance)
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ["student", "bonus"]
+    list_editable = ["bonus"]
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ["course", "student"]
