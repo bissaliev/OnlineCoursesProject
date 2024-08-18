84fd821b6a998fd3d877616f1d3c4e1c342d5903
bissaliev <bissaliev21@gmail.com>
2024-08-18 07:46:04 +0400

    Добавление select_related('balance') в UserViewSet

--- product/api/v1/views/user_view.py
+++ product/api/v1/views/user_view.py
@@ -1,13 +1,12 @@
+from api.v1.serializers.user_serializer import CustomUserSerializer
 from django.contrib.auth import get_user_model
 from rest_framework import permissions, viewsets
-
-from api.v1.serializers.user_serializer import CustomUserSerializer
 
 User = get_user_model()
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.select_related("balance")
     serializer_class = CustomUserSerializer
     http_method_names = ["get", "head", "options"]
     permission_classes = (permissions.IsAdminUser,)
